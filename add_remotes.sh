eb2f0e28f68a55378a6515c307c15d1e45641424
Nathan Greenstein <nathang345@gmail.com>
2019-03-25 14:47:08 -0400

    added my fork to the add_remotes.sh script

--- add_remotes.sh
+++ add_remotes.sh
@@ -15,3 +15,4 @@ git remote add ellie https://github.com/ebennett10/storytelling-with-data.git
 git remote add cameron https://github.com/camartin95/storytelling-with-data.git
 git remote add isabelle https://github.com/igleonaitis/storytelling-with-data.git
 git remote add amara https://github.com/amaragordon/storytelling-with-data.git
+git remote add nathan https://github.com/ngreenstein/storytelling-with-data.git
